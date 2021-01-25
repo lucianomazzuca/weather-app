245d2db25b45df6c7b1bbb237d6656d29d07c172
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2021-01-25 20:22:33 -0300

    Refactor style of unit button
    Changed design to switch between celsius and fahrenheit

--- src/components/UnitButton.js
+++ src/components/UnitButton.js
@@ -1,13 +1,19 @@
+import '../assets/css/unit_button.css'
+
 const UnitButton = ({ unit, changeUnit }) => {
   return (
-    <>
-      <button 
+    <div className="flex justify-center items-center mt-6">
+      {/* <button 
         className="border border-gray-500 rounded-lg text-lg bg-gray-200 mt-4 px-2 inline-block mx-auto"
         onClick={(e) => changeUnit(e)}
       >
         {unit}
-      </button>
-    </>
+      </button> */}
+
+      <span className="text-2xl mr-2 font-semibold" style={{color: unit==='metric' ? '#e8661c' : 'black'}}>C°</span>
+      <input type="checkbox" onClick={changeUnit} className="inline-block mt-1" />
+      <span className="text-2xl ml-2 font-semibold" style={{color: unit==='imperial' ? 'royalblue' : 'black'}}>F°</span>
+    </div>
   );
 };
 
